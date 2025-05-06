8607af10bc4f30b7f449bf074df89a2eeb1d559a
Jordan Guillou <jordanguillou@hotmail.co.uk>
2025-05-06 23:11:31 +0100

    Updated base website

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("toggle-form");
+    const form = document.getElementById("consultation-form");
+
+    toggle.addEventListener("click", function () {
+        form.classList.toggle("open");
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
